bb2da1ee7cb817c90e5786343d179ae0c51db1ab
Artem Sementsov <artem.sementsov@gmail.com>
2019-05-09 16:34:58 +0300

    PartialFunction.scala PatternMatching.scala

---
+++ src/main/scala/part5/PartialFunction.scala
@@ -0,0 +1,47 @@
+package part5
+
+object PartialFunction extends App {
+
+  case class Jar(name: String, value: Int, price: Double)
+
+  def discount: PartialFunction[Jar, String] = {
+    case Jar(name, value, price) if value > 10 => name + " " + price * 0.1
+    case Jar(name, value, price) if value >= 5 => name + " " + price * 0.05
+  }
+
+  List(
+    Jar("Морской синий 6л", 6, 3000.0),
+    Jar("Огненно-красный 12л", 12, 5000.0),
+    Jar("Зеленый 1л", 1, 500.0)
+  )
+
+  val jars = List(Jar("Морской синий 6л", 6, 3000), Jar("Огненно-красный 12л", 12, 5000))
+  println(jars.collect(discount))
+
+  {
+    def swap3(tuple: (Int, Int, Int)): (Int, Int, Int) = tuple match {
+      case (a, b, c) => (c, b, a)
+    }
+
+    println(swap3((1, 2, 3)))
+    //    require(swap3((1, 2, 3)) == (3, 2, 1))
+  }
+
+  {
+
+    def foo(list: List[Int]): Int = {
+      list.find(x => x % 3 == 0).map(x => x * 2).getOrElse(0)
+    }
+
+    require(foo(List(1, 2, 3, 4, 5, 6)) == 6)
+  }
+
+  {
+
+    def bar: Int => Option[Int] = ???
+    val x: Option[Int] = Some(10)
+
+    println(x.map(x => bar(x)).getOrElse(None))
+  }
+
+}
--- src/main/scala/part5/PatternMatching.scala
+++ src/main/scala/part5/PatternMatching.scala
@@ -1,5 +1,57 @@
 package part5
 
-object PatternMatching {
+import sun.awt.AWTAccessor.InputEventAccessor
 
+import scala.util.matching.Regex
+
+object PatternMatching extends App {
+
+  {
+    case class Pet(name: String, says: String)
+
+    def matching(pet: Pet): String = {
+      val robotSays = "(.*[01]+.*)".r
+      val kind = pet match {
+        case Pet("Rex", _) => "dog"
+        case Pet(_, "meow" | "nya") => "cat"
+        case Pet(_, robotSays(_)) => "robot"
+        case Pet(_, _) => "unknown"
+      }
+
+      println(kind)
+      kind
+    }
+
+    //  require(matching(Pet("Rex", "rrrr")) == "dog")
+    //  require(matching(Pet("kitty", "meow")) == "cat")
+    //  require(matching(Pet("kit", "nya")) == "cat")
+    //  require(matching(Pet("R2D2", "0101010101meow0111010001")) == "robot")
+
+  }
+
+  {
+    def testMatching(input: List[String]) = {
+      val nameR = "([a-zA-Z]{1,})".r
+      val nameEmailR = "([a-zA-Z]+) (\\w+@)(\\w+\\.\\w+)".r
+      val emailR = "(\\w+@)(\\w+\\.\\w+)".r
+      val result = input match {
+        case List(nameR(str), rest@_*) => {
+          val email = rest.head
+          println(email)
+          str + " " + email match {
+            case emailR(parts@_*) => parts.tail.head
+            case _ => "invalid1"
+          }
+        }
+        case List(nameEmailR(parts@_*)) => parts.head + " " + parts.tail.tail.head
+        case _ => "invalid"
+      }
+      println(result)
+    }
+    println("artem".matches("([a-zA-Z]{1,})"))
+    println("artem@gmail.com".matches("(\\w+@)(\\w+\\.\\w+)"))
+    println(testMatching(List("artem", "artem@gmail.com")))
+    println(testMatching(List("artem artem@gmail.com")))
+
+  }
 }
